44718f1c9ea7e4c1fd8eb77f1f1d225bf7bb9c73
3RaghavK3 <33raghavk33@gmail.com>
2026-07-17 21:47:41 +0530

    :backend setup eslint prettier express ts

---
+++ backend/src/server.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Application = express();
+const port: number = Number(process.env.PORT) ||  3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

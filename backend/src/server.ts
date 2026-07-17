d3346f67c10fc0a43047dc44fe759dc2b3e7e870
3RaghavK3 <33raghavk33@gmail.com>
2026-07-17 23:30:37 +0530

    :chore:redis and db setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,51 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from "dotenv";
-import client from './config/db';
+import pool from './config/db.js';
+import redis from './config/redis.js';
 
 dotenv.config();
 
 const app: Application = express();
 const port: number = Number(process.env.PORT) ||  3000;
 
-client.connect()
-    .then(()=>{
+const dbcheck = async()=>{
+    try{
+        await pool.query("SELECT 1");
         console.log("Connected to the database");
+        return true;
+    }
+    catch(e){
+        console.error("Connection to the database failed",e);
+        return false;
+    }
 
-        app.listen(port, ()=>{
-            console.log(`Server is running on ${port}`)
-        });
-})
-    .catch((e)=>{
-        console.error(e);
-    });
+}
+
+const rdcheck=async()=>{
+    try{
+        await redis.ping();
+        console.log("Connected to redis");
+        return true;
+    }
+    catch(e){
+        console.error("Connection to the redis failed",e);
+        return false;
+    }
+    
+}
+
+async function start(){
+    const [db,redis]=await Promise.all([dbcheck(),rdcheck()]);
+
+    if(db && redis){
+        app.listen(port,()=>{
+            console.log(`Server running on port :${port}`)
+        })
+    }
+    else{
+        console.log("Startup Failed..")
+        process.exit(1);
+    }
+}
+
+start();

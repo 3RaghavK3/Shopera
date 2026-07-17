5145b4dc91f6408581ead4111099a47995d4eb51
3RaghavK3 <33raghavk33@gmail.com>
2026-07-17 22:21:07 +0530

    :backend connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,20 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from "dotenv";
+import client from './config/db';
 
 dotenv.config();
 
 const app: Application = express();
 const port: number = Number(process.env.PORT) ||  3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
-});
+client.connect()
+    .then(()=>{
+        console.log("Connected to the database");
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+        app.listen(port, ()=>{
+            console.log(`Server is running on ${port}`)
+        });
+})
+    .catch((e)=>{
+        console.error(e);
+    });
